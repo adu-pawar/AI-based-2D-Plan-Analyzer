5e8a55c7c949824e9ed514a297aa391434179430
ADITYA PAWAR <adu.pawar.1@gmail.com>
2026-08-16 17:25:51 +0530

    Update API endpoint for file upload

--- frontend/src/components/uplodeFile.jsx
+++ frontend/src/components/uplodeFile.jsx
@@ -29,7 +29,7 @@ export default function UploadFile() {
     formData.append("file", file);
 
     try {
-      const res = await axios.post("http://localhost:8000/api/check", formData);
+      const res = await axios.post("https://ai-based-2d-plan-analyzer.onrender.com/api/check", formData);
 
       setApiData(res.data);
       setErr("");
